076a37262911ba391c9e6336e170b7559be00dbb
Ramprasad Murahari <120369693+Kannatech@users.noreply.github.com>
2022-12-14 17:04:00 +0530

    Update FindingIP.java

--- Finding IP/src/FindingIP.java
+++ Finding IP/src/FindingIP.java
@@ -11,9 +11,10 @@ public class FindingIP extends JFrame implements ActionListener{
         l.setBounds(50,70,150,20);;
         tf=new JTextField();
         tf.setBounds(50,100,200,20);
-
+        tf.setBackground(Color.lightGray);
         b=new JButton("Find IP");
         b.setBounds(50,150,80,30);
+        b.setBackground(Color.pink);
         b.addActionListener(this);
         add(l);
         add(tf);
